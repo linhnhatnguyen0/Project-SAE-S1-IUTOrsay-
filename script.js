2056e23c5647511a0292810767cc7f1ece4f62c2
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2021-10-30 02:50:48 +0200

    0250-301021

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0px";
-  } else {
-    document.getElementById("header").style.top = "-150px";
-  }
-  prevScrollpos = currentScrollPos;
-};
-
-var timer;
-var delay = 2000;
 $(document).ready(function () {
-  $(".name-team").css("opacity", 0);
-  $("button.show-name").each(function () {
-    $(this).on("click", function () {
-      $(this).parent().parent().toggleClass("hide1");
-      $(this).parent().parent().children(".name-team").toggleClass("hide2");
-      if (
-        $(this).parent().parent().children(".name-team").css("opacity") == 0
-      ) {
-        $(this).parent().parent().children(".name-team").fadeTo(100, 1);
-      }
-      if (
-        $(this).parent().parent().children(".name-team").css("opacity") == 1
-      ) {
-        $(this).parent().parent().children(".name-team").fadeTo(100, 0);
-      }
-    });
-  });
   $("#nav-button").on("click", function () {
     $(this).parent().children(".nav-mid-element").toggleClass("hide-element");
     $(this).parent().parent().toggleClass("height-nav");
@@ -41,22 +11,4 @@ $(document).ready(function () {
       $(this).children("ul").css("display", "none");
     }
   );
-});
-
-$(window).on("load", function () {
-  $(window)
-    .on("scroll", function () {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      var objectBottom =
-        $(".description").offset().top + $(".description").outerHeight();
-      if (objectBottom < windowBottom) {
-        //object comes into view (scrolling down)
-        $("#header").css("background-color", "#071336");
-        $("#header").css("box-shadow", "0 0 10px 0 #000");
-      } else {
-        $("#header").css("box-shadow", "unset");
-        $("#header").css("background-color", "transparent");
-      }
-    })
-    .on("scroll");
 });
