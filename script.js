ab5ae9b0f56a8853ea4d283a1d0c38c2b343eac3
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2021-11-13 00:47:42 +0100

    0047-131121

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ $(document).ready(function () {
       $(this).children("ul").css("display", "none");
     }
   );
-  var textHeight = $(".cours-img").children("img").innerHeight();
-  console.log(textHeight);
-  $(".cours-text").css("height", textHeight);
+
+  $(".container>input").click(function () {
+    $(".container>input").each(function () {
+      var name = $(this).prop("class");
+      if ($(this).prop("checked") == true) {
+        $("." + name).show();
+      } else {
+        $("." + name).hide();
+        $(this).css("display", "unset");
+      }
+    });
+  });
+  if ($(".cours-list-element").css("display") == "flex") {
+    var textHeight = $(".cours-img").children("img").innerHeight();
+    $(".cours-text").css("height", textHeight);
+  }
+  $(window).resize(function () {
+    var width = $(window).width();
+    if (width < 1023) {
+      $(".cours-text").css("height", "unset");
+    }
+  });
 });
